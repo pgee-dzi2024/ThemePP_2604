626ee0e2d85e2b37e5c889e1ec7b3a7ff1089226
pgee_ <imariq77@gmail.com>
2026-03-23 10:35:35 +0200

    стартиране на шаблон

--- DP/main/views.py
+++ DP/main/views.py
@@ -1,9 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from .serializers import DocumentSerializer
-import pytesseract
-from PIL import Image
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def index(request):
+    return HttpResponse("РАБОТИ")
+
+def index(request):
+    return render(request, 'main/index.html')
 
 
 # Ако сте на Windows и получите грешка, че Tesseract не е намерен,
