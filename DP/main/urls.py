d371138cfecf15b73617c597d4f3d0ef8844e96a
Георги Бориков <pgee_dzi2024@abv.bg>
2026-04-07 12:30:08 +0300

    приложение с шаблон

--- DP/main/urls.py
+++ DP/main/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     # Използваме вградения TemplateView, за да заредим директно HTML файла,
     # който ще създадем в следващата стъпка.
     path('', TemplateView.as_view(template_name='main/index.html'), name='home'),
+    path('old', TemplateView.as_view(template_name='main/index_old.html'), name='home'),
 
     # 2. Път за API-то, което приема снимката и връща извлечения текст (Backend)
     path('api/upload/', DocumentUploadView.as_view(), name='api-document-upload'),
